15e6160a4a62f1f78c953cac1deb765b7175d5ac
silven5 <elvenff@gmail.com>
2023-11-21 09:51:25 +0200

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
-
+import { Animal } from './animal';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'myApp';
+  name: string = 'Список чарівних тварин';
+  animals!: Animal[];
+  async populate() {
+    const requestURL = 'https://api.jsonbin.io/v3/b/654a3dd712a5d376599618c7';
+    const request = new Request(requestURL);
+    const response = await fetch(request);
+    if (response.ok) {
+      const ani = await response.json();
+      this.animals = ani.record;
+    } else alert('Помилка');
+  }
+  //Викликаємо при завантажені сторінки
+  ngOnInit() {
+    this.populate();
+  }
 }
